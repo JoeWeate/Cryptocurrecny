79dc611d187fcd8f8fdf1abff67b6e6dd8319389
JoeWeate <jfw6fp@virginia.edu>
2015-01-30 15:10:27 -0500

    Update keypair.go

--- keypair.go
+++ keypair.go
@@ -74,7 +74,7 @@ func generateVanityAddress(pattern string) (*btcec.PublicKey, *btcec.PrivateKey)
 }
 func main() {
 
-	generateVanityAddress("JOE")
+	generateVanityAddress("JW")
 	// In order to recieve coins we must generate a public/private key pair.
 
 	//pub, priv := generateKeyPair()
